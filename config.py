d083b2a467f71510f05b0e71768d540033f2bc18
VadimIgnatev <111434975+VadimIgnatev@users.noreply.github.com>
2025-06-14 19:21:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-# берем либо стандартный DATABASE_URL (если вы все же его связали),
-# либо подставляем Railway Provided
 DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("RAILWAY_POSTGRESQL_URL")
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN не задан в окружении")
 if not DATABASE_URL:
-    raise RuntimeError(
-        "DATABASE_URL и RAILWAY_POSTGRESQL_URL не найдены — проверьте переменные Railway"
-    )
+    raise RuntimeError("DATABASE_URL / RAILWAY_POSTGRESQL_URL не найдены")
